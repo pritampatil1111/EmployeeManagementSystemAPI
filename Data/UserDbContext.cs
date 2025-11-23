c09f83df080a83d857e5875e20a182da9949efb7
pritam patil <59310441+pritampatil1111@users.noreply.github.com>
2025-11-23 08:27:21 +0530

    Add UserDbContext with Users DbSet for EF Core

---
+++ Data/UserDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using EmployeeManagementSystemAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystemAPI.Data
+{
+    public class UserDbContext : DbContext
+    {
+        // Constructor to accept DbContextOptions
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+
+        // DbSet for Users table
+        public DbSet<User> Users { get; set; }
+      
+    }
+}
